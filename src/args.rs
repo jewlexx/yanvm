3cb5faffd7df07e784f00ef0ce212aa614e339b1
Juliette Cordor <grassojames5@gmail.com>
2022-07-10 11:56:42 +1000

    added dependabot config

--- src/args.rs
+++ src/args.rs
@@ -3,5 +3,11 @@ use clap::Parser;
 #[derive(Parser)]
 #[clap(name = "Yet Another Node Version Manager", about)]
 pub struct Args {
-    pub command: Option<String>,
+    pub command: Option<Commands>,
+}
+
+#[derive(Debug, Parser)]
+pub enum Commands {
+    #[clap(subcommand)]
+    Install,
 }
