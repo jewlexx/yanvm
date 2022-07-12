ea0923abb51704f0c84107d5e266b477b784775c
Juliette Cordor <grassojames5@gmail.com>
2022-07-13 09:05:48 +1000

    added win c file and added it to build script

--- build.rs
+++ build.rs
@@ -1,1 +1,12 @@
+use std::{env::current_dir, fs::read_dir};
+
+#[cfg(windows)]
+fn main() {
+    let cwd = current_dir().expect("failed to get current directory");
+
+    // The directory containing the C files for Windows specific code
+    let win_dir = cwd.join("lib/win");
+}
+
+#[cfg(not(windows))]
 fn main() {}
