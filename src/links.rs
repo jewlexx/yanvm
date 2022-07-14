572588a4b5b650d21cba447bd6b83c9db30ba47b
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-07-14 01:52:43 +0000

    fix

--- src/links.rs
+++ src/links.rs
@@ -1,5 +1,7 @@
 use std::path::Path;
 
+// NOTE: These should only be used to symlink one level directories
+
 #[cfg(windows)]
 pub fn symlink_dir<P>(original: P, target: P)
 where
@@ -12,4 +14,8 @@ pub fn symlink_dir<P>(original: P, target: P)
 where
     P: AsRef<Path>,
 {
+    use std::fs::read_dir;
+    use std::os::unix::fs::symlink;
+
+    let og_dir = read_dir(original);
 }
